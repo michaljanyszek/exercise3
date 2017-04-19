807ea96f804b8e2d468d6c4e10ace4065d0ed60f
Michał Janyszek <michal.janyszek94@gmail.com>
2017-04-19 19:52:57 +0200

    Added homework

--- client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
+++ client/src/main/java/wdsr/exercise3/hr/HolidayClient.java
@@ -3,17 +3,27 @@ package wdsr.exercise3.hr;
 import java.net.URL;
 import java.util.Date;
 
+import javax.xml.datatype.DatatypeConfigurationException;
+
+import wdsr.exercise3.hr.ws.EmployeeType;
+import wdsr.exercise3.hr.ws.HolidayRequest;
+import wdsr.exercise3.hr.ws.HolidayResponse;
+import wdsr.exercise3.hr.ws.HolidayType;
+import wdsr.exercise3.hr.ws.HumanResource;
+import wdsr.exercise3.hr.ws.HumanResourceService;
+
 // TODO Complete this class to book holidays by issuing a request to Human Resource web service.
 // In order to see definition of the Human Resource web service:
 // 1. Run HolidayServerApp.
 // 2. Go to http://localhost:8090/holidayService/?wsdl
 public class HolidayClient {
-	/**
+	private HumanResourceService humanResourceService;
+    /**
 	 * Creates this object
 	 * @param wsdlLocation URL of the Human Resource web service WSDL
 	 */
 	public HolidayClient(URL wsdlLocation) {
-		// TODO
+		humanResourceService = new HumanResourceService(wsdlLocation);
 	}
 	
 	/**
@@ -27,7 +37,40 @@ public class HolidayClient {
 	 * @throws ProcessingException if request processing fails.
 	 */
 	public int bookHoliday(int employeeId, String firstName, String lastName, Date startDate, Date endDate) throws ProcessingException {
-		// TODO
-		return 0;
+        HumanResource humanResource = humanResourceService.getHumanResourcePort();
+        try {
+            HolidayResponse holidayResponse;
+            holidayResponse = humanResource.holiday(createHolidayRequest(employeeId, firstName, lastName, startDate, endDate));
+            return holidayResponse.getRequestId();
+        } catch (Exception e) {
+            throw new ProcessingException();
+        }
+	}
+	
+	private HolidayRequest createHolidayRequest(int employeeId, String firstName, String lastName, Date startDate,
+			Date endDate) throws DatatypeConfigurationException {
+		HolidayRequest holidayRequest = new HolidayRequest();
+		holidayRequest.setEmployee(createHolidayEmployee(employeeId, firstName, lastName));
+		holidayRequest.setHoliday(createHoliday(startDate, endDate));
+		
+		return holidayRequest;
+	}
+
+	private EmployeeType createHolidayEmployee(int employeeId, String firstName, String lastName) {
+		EmployeeType employeeType = new EmployeeType();
+		employeeType.setNumber(employeeId);
+		employeeType.setFirstName(firstName);
+		employeeType.setLastName(lastName);
+		
+		return employeeType;
+	}
+
+	private HolidayType createHoliday(Date startDate, Date endDate)
+			throws DatatypeConfigurationException {
+		HolidayType holidayType = new HolidayType();
+		holidayType.setStartDate(startDate);
+		holidayType.setEndDate(endDate);
+		
+		return holidayType;
 	}
 }
